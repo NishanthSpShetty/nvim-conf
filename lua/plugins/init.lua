399a0e140e3cf746e19078f41c069993450eafb8
NishanthSpShetty <nishanthspshetty@gmail.com>
2025-11-30 23:31:07 +0530

    fix: update color scheme
    Signed-off-by: NishanthSpShetty <nishanthspshetty@gmail.com>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,6 @@
 return {
     { "ellisonleao/gruvbox.nvim",        priority = 1000,   config = true,   opts = ... },
     { "folke/tokyonight.nvim",           lazy = false,      priority = 1000, opts = {} },
-    { "rebelot/kanagawa.nvim" },
     --colorscheme { "EdenEast/nightfox.nvim" },
     { "nvim-treesitter/nvim-treesitter", branch = 'master', lazy = false,    build = ":TSUpdate" },
     { "tpope/vim-fugitive" },
